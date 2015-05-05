1384087629aa629216d87932b6189251954f5272
nwparker <nwp7@cornell.edu>
2015-05-05 01:49:20 -0400

    Reduce Phase

--- p2_blocked_mapreduce/Reduce.java
+++ p2_blocked_mapreduce/Reduce.java
@@ -6,6 +6,20 @@ import org.apache.hadoop.mapreduce.Reducer;
  * IN: node id -> list of rank flows
  * OUT: node id -> pagerank
  */
-public class Reduce extends Reducer<IntWritable, FloatWritable, IntWritable, FloatWritable> {
-
+public class Reduce extends Reducer<IntWritable, Node, IntWritable, FloatWritable> {
+	
+    public void reduce(IntWritable id, Iterable<Node> nodes, Context context)
+    throws IOException, InterruptedException {
+    	HashMap<Integer, Float> node_ranks = pagerankBoundaries(nodes);
+    	
+    	for(Map.Entry<Integer, Float> entry : node_ranks.entrySet()){
+    		IntWritable nodeID = new IntWritable(entry.getKey());
+    	    FloatWritable nodePageRank = new FloatWritable(entry.getValue());
+    	    context.write(nodeID, nodePageRank);
+    	}
+    	
+    }
 }
+
+
+
