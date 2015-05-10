e7b4a9a28297885a71b9ccabbcf459463f66571d
mzw4 <mzw4@cornell.edu>
2015-05-10 13:47:26 -0400

    main file combiner

--- p2_mapreduce/SimpleMapReduce.java
+++ p2_mapreduce/SimpleMapReduce.java
@@ -23,11 +23,13 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.log4j.BasicConfigurator;
 
+import com.amazonaws.services.elastictranscoder.model.JobOutput;
+
 public class SimpleMapReduce {
-    public enum Counter {
-    	RESIDUALS
-    }
-    
+	public enum Counter {
+		RESIDUALS
+	}
+
 	private static final int MAX_ITERATIONS = 100;
 	private static final double CONVERGENCE_THRESHOLD = 0.001;
 	private static final Log LOG = LogFactory.getLog(SimpleMapReduce.class);
@@ -35,120 +37,128 @@ public class SimpleMapReduce {
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		BasicConfigurator.configure();
-//		Path outputDir = new Path(args[1]);
-//		Path inputPath = new Path(outputDir, "nodes_simple.txt");
+		// Path outputDir = new Path(args[1]);
+		// Path inputPath = new Path(outputDir, "nodes_simple.txt");
 
 		// temp
 		Path outputDir = new Path("output");
+		Path inputDir = new Path("input");
 		Path inputPath = new Path("nodes_simple.txt");
-		Path inputFile = new Path("nodes_simple.txt");
+		Path originalInput = inputPath;
+		// Path inputFile = new Path("nodes_simple.txt");
 
-//		outputDir.getFileSystem(conf).delete(outputDir, true);
-//		outputDir.getFileSystem(conf).mkdirs(outputDir);
-		
-		int numNodes = createInputFile(inputFile, inputPath);
-		
+		// outputDir.getFileSystem(conf).delete(outputDir, true);
+		// outputDir.getFileSystem(conf).mkdirs(outputDir);
+
+		int numNodes = getNumNodes(originalInput);
+
 		boolean converged = false;
-	    int iter = 1;
-	    while (!converged) {
-	      Path jobOutputPath = new Path(outputDir, String.valueOf(iter));
-	      jobOutputPath.getFileSystem(conf).delete(jobOutputPath, true);
-	      
-	      System.out.println("======================================");
-	      System.out.println("=  Iteration:    " + iter);
-	      System.out.println("=  Input path:   " + inputPath);
-	      System.out.println("=  Output path:  " + jobOutputPath);
-	      System.out.println("======================================");
+		int iter = 0;
+		while (!converged) {
+			Path jobOutputPath = new Path(outputDir, String.valueOf(iter));
+			jobOutputPath.getFileSystem(conf).delete(jobOutputPath, true);
 
-	      converged = calcPageRank(inputPath, jobOutputPath, numNodes) < CONVERGENCE_THRESHOLD * numNodes;
-	      inputPath = jobOutputPath;
-	      iter++;
-	    }
-		System.out.println("Convergence is below " + CONVERGENCE_THRESHOLD + ", we're done");
+			System.out.println("======================================");
+			System.out.println("=  Iteration:    " + iter);
+			System.out.println("=  Input path:   " + inputPath);
+			System.out.println("=  Output path:  " + jobOutputPath);
+			System.out.println("======================================");
+
+			converged = calcPageRank(inputPath, jobOutputPath, numNodes) < CONVERGENCE_THRESHOLD
+					* numNodes;
+			// inputPath = jobOutputPath;
+			iter++;
+			inputPath = new Path(inputDir, String.valueOf(iter));
+			inputPath.getFileSystem(conf).delete(inputPath, true);
+			
+			formatInputFile(originalInput, jobOutputPath, inputPath);
+		}
+		System.out.println("Convergence is below " + CONVERGENCE_THRESHOLD
+				+ ", we're done");
+	}
+
+	public static int getNumNodes(Path path) throws IOException {
+		Configuration conf = new Configuration();
+		FileSystem fs = path.getFileSystem(conf);
+		List<String> lines = IOUtils.readLines(fs.open(path));
+		return lines.size();
 	}
 
 	/*
-	 * Create the input file for the mapper from our original nodes.txt
+	 * Create the input file for the mapper from our original nodes.txt and
+	 * updated pageranks
 	 */
-	public static int createInputFile(Path nodes_file, Path targetFile)
-			throws IOException {
+	public static int formatInputFile(Path originalInput, Path prevOutput,
+			Path newInput) throws IOException {
 		Configuration conf = new Configuration();
-		FileSystem fs = nodes_file.getFileSystem(conf);
-//		OutputStream os = fs.create(targetFile);
-		List<String> node_lines = IOUtils.readLines(fs.open(nodes_file), "UTF8");
-		int num_nodes = node_lines.size();
-		System.out.println(node_lines.size());
-		return num_nodes;
-		
-//		float initial_rank = 1.0f / num_nodes;
-//		
-//		// parse all nodes into Hadoop writeable format
-//		for(int i = 0; i < num_nodes; i++) {
-//			if(i % 100000 == 0) {
-//				System.out.println("writing node " + i);
-//			}
-//			String[] node_vals = node_lines.get(i).split("\\$");
-//			if (node_vals.length < 2) {
-//				System.out.println("Line format was mezzed uppp.");
-//				continue;
-//			}
-//			int id = Integer.parseInt(node_vals[0]);
-//			
-//			// parse outgoing edges
-//			String[] outgoing_vals = node_vals[1].split(" ");
-//			int[] outgoing = new int[outgoing_vals.length];
-//			for(int k = 0; k < outgoing.length; k++) {
-//				outgoing[k] = Integer.parseInt(outgoing_vals[k]);
-//			}
-//			
-//			DataOutputBuffer buffer = new DataOutputBuffer();
-//
-//			Node node = new Node(id, initial_rank, outgoing, null);
-//			node.write(buffer);
-//			buffer.writeTo(os);
-//		}
-//
-//		os.close();
-//		return num_nodes;
+		FileSystem fs = prevOutput.getFileSystem(conf);
+		OutputStream os = fs.create(newInput);
+
+		// Read files
+		List<String> originalNodes = IOUtils.readLines(fs.open(originalInput),
+				"UTF8");
+		List<String> prevOutputNodes = IOUtils.readLines(
+				fs.open(originalInput), "UTF8");
+
+		// parse all nodes into Hadoop writeable format
+		for (int i = 0; i < originalNodes.size(); i++) {
+			if (i % 100000 == 0) {
+				System.out.println("writing node " + i);
+			}
+			String originalLine = originalNodes.get(i).trim();
+			String[] prevVals = prevOutputNodes.get(i).split("\\$");
+
+			if (prevVals.length < 2) {
+				System.out.println("Line format was mezzed uppp.");
+				continue;
+			}
+
+			// Add the ranks to the original line
+			String newLine = originalLine + "$" + prevVals[1];
+			IOUtils.write(newLine, os);
+		}
+
+		os.close();
+		return originalNodes.size();
 	}
 
 	public static double calcPageRank(Path inputPath, Path outputPath,
 			int numNodes) throws Exception {
 		Configuration conf = new Configuration();
 		conf.setInt("num_nodes", numNodes);
-		
+
 		Job job = Job.getInstance(conf, "SimpleMapReduce");
 		job.setJarByClass(SimpleMapReduce.class);
 		job.setMapperClass(Map.class);
 		job.setReducerClass(Reduce.class);
-		
+
 		job.setOutputKeyClass(IntWritable.class);
 		job.setOutputValueClass(FloatWritable.class);
 
 		job.setInputFormatClass(TextInputFormat.class);
-//		job.setInputFormatClass(SequenceFileAsBinaryInputFormat.class);
+		// job.setInputFormatClass(SequenceFileAsBinaryInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 
 		FileInputFormat.addInputPath(job, inputPath);
 		FileOutputFormat.setOutputPath(job, outputPath);
-		
-//		job.setInputFormatClass(KeyValueTextInputFormat.class);
-//		job.setMapOutputKeyClass(Text.class);
-//		job.setMapOutputValueClass(Text.class);
-		
+
+		// job.setInputFormatClass(KeyValueTextInputFormat.class);
+		// job.setMapOutputKeyClass(Text.class);
+		// job.setMapOutputValueClass(Text.class);
+
 		if (!job.waitForCompletion(true)) {
 			throw new Exception("Something went wrong with the job");
 		}
-		org.apache.hadoop.mapreduce.Counter residuals = job.getCounters().findCounter(Counter.RESIDUALS);
-		long convergence = residuals.getValue();
-		residuals.setValue(0);
+
+		long convergence = job.getCounters().findCounter(Counter.RESIDUALS)
+				.getValue();
 
 		System.out.println("======================================");
 		System.out.println("=  Num nodes:           " + numNodes);
-//		System.out.println("=  Summed convergence:  " + total_convergence);
+		// System.out.println("=  Summed convergence:  " + total_convergence);
 		System.out.println("=  Convergence:         " + convergence);
 		System.out.println("======================================");
-		
+
 		return convergence;
 	}
 
